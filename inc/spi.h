b3a5ab8350f6da395757157b135df9c464a06b40
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-17 19:43:53 +0000

    Add initial setup for STM32 SPI
    - Mainly some skeleton code to get an idea of what needs intialising and
      what doesn't

--- inc/spi.h
+++ inc/spi.h
@@ -6,5 +6,7 @@
 void assert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
 void deassert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
 
+void setup_hw_spi(void);
+
 
 #endif /* SPI_H */
