b662f1c24feae87c0e4df726fecb4d5037999cff
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-18 12:06:03 +0000

    Simplify stm32 delay function using prescaler inputs
    - Rather than changing the count for a 1us delay we can use the
      prescaler timer input to adjust the speed of the delay
    - This makes it easier to use the delay function as we only need to
      supply a us delay rather than a scaler and a us delay
    - No bounds checks at the moment so you could input a 0 for the clock
      divider and get the unsigned int wrap around making the prescaler
      UINT_MAX

--- inc/stm32g4xx_timers.h
+++ inc/stm32g4xx_timers.h
@@ -1,7 +1,7 @@
 #ifndef STM32G4xx_TIMERS_H
 #define STM32G4xx_TIMERS_H
 
-void timer_setup(void);
-void stm32_delay_us(unsigned int hclk_clock_divider);
+void timer_setup(unsigned int hclk_clock_divider);
+void stm32_delay_us(unsigned int us_count);
 
 #endif /* STM32G4xx_TIMERS_H */
--- src/stm32g4xx_timers.c
+++ src/stm32g4xx_timers.c
@@ -3,23 +3,23 @@
 #include "stm32g4xx.h"
 #include "stm32g4xx_ll_spi.h"
 
-void timer_setup(void)
+void timer_setup(unsigned int hclk_clock_divider)
 {
+	// Clock division = 1/(PSC + 1) as counting starts from 0
 	__TIM2_CLK_ENABLE();
 	TIM2->CR1 |= TIM_CR1_DIR; // Down counter
 	TIM2->CNT = 0;
-	TIM2->PSC = 0;
+	TIM2->PSC = hclk_clock_divider - 1; // prescalers should be powers of 2
 	TIM2->ARR = 0;
 }
 
 // us delay needed for HW reset of ST7789 display
-void stm32_delay_us(unsigned int hclk_clock_divider)
+void stm32_delay_us(unsigned int us_count)
 {
 	unsigned int delay_count = 15; // for a 1 us delay (minimum)
-	delay_count = 15 * hclk_clock_divider;
+	delay_count = 15 * us_count;
 
 	TIM2->CNT = delay_count;
-	TIM2->PSC = 0;
 	TIM2->ARR = delay_count;
 
 	// Enable timer
