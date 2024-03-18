d4ef4d63f85d9edd3de9ff551076cc402c668f4c
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-18 08:33:41 +0000

    Add initial code for timer setup
    - Tested toggling a LED on/off with this code fragment and it works
    - Note: It is missing the RCC clock enable for that timer port, just a
      basic setup for some future tests/mocks

--- inc/spi.h
+++ inc/spi.h
@@ -7,6 +7,7 @@ void assert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
 void deassert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
 
 void setup_hw_spi(void);
+void delay_us(unsigned int hclk_clock_divider);
 
 
 #endif /* SPI_H */
--- src/spi.c
+++ src/spi.c
@@ -34,3 +34,28 @@ void setup_hw_spi(void)
 
 	// Enable SPI module once setup is complete
 }
+
+// 5-9 us for RST delay (HW reset)
+// Display is then blanked for 120ms
+void delay_us(unsigned int hclk_clock_divider)
+{
+	unsigned int delay_count = 15; // for a 1 us delay (minimum)
+	delay_count = 15 * hclk_clock_divider; // for a 1 us delay (minimum)
+
+	TIM2->CR1 |= TIM_CR1_DIR; // Down counter
+	TIM2->CNT = delay_count;
+	TIM2->PSC = 0;
+	TIM2->ARR = delay_count;
+
+	// Enable timer
+	TIM2->CR1 |= TIM_CR1_CEN;
+
+	while (!(TIM2->SR & TIM_SR_UIF)) {
+		// delay
+	}
+
+	TIM2->SR &= ~TIM_SR_UIF;
+
+	// Disable timer
+	TIM2->CR1 &= ~TIM_CR1_CEN;
+}
