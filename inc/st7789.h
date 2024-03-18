99d6eb195dbb4363bd2182206d0b2dba157702f8
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-18 09:22:10 +0000

    Add initial code for a ST7789 HW reset

---
+++ inc/st7789.h
@@ -0,0 +1,9 @@
+#ifndef ST7789_H
+#define ST7789_H
+
+#include <stdint.h>
+
+void st7789_hw_reset(uint32_t* res_addr, unsigned int res_pin);
+
+
+#endif /* ST7789_H */
---
+++ src/st7789.c
@@ -0,0 +1,13 @@
+#include "st7789.h"
+#include "spi.h"
+
+void st7789_hw_reset(uint32_t* res_addr, unsigned int res_pin)
+{
+	// Must be a hi-lo transition, pulse RES for 10us minimum
+	// Ignored in sleep-in mode
+	assert_spi_pin(res_addr, res_pin);
+	delay_us(15);
+	deassert_spi_pin(res_addr, res_pin);
+	delay_us(10);
+	assert_spi_pin(res_addr, res_pin);
+}
