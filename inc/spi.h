aa05e2f7f144beba4a117fdbb4a7e712022bb7d0
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-19 12:42:47 +0000

    Removed unused prototype function

--- inc/spi.h
+++ inc/spi.h
@@ -6,6 +6,4 @@
 void assert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
 void deassert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
 
-void setup_hw_spi(void);
-
 #endif /* SPI_H */
