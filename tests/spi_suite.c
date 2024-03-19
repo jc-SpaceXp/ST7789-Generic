0a3f6b8e77994d5d42b5a6653956465b891ba1cf
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-19 08:35:05 +0000

    Refactor tests to use a looped version (SPI)

--- tests/spi_suite.c
+++ tests/spi_suite.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #include "greatest.h"
 #include "spi_suite.h"
@@ -7,39 +9,61 @@
 
 static uint32_t some_gpio_port = 0xFFFFFFFF;
 
-TEST test_write_spi_gpio_pin_0_high(void)
+
+TEST test_write_spi_gpio_pins_high(unsigned int pin)
 {
-	some_gpio_port = 0;
-	unsigned int pin = 0;
+	uint32_t init_val = 0x10000000;
+	some_gpio_port = init_val;
 	assert_spi_pin(&some_gpio_port, pin);
-	ASSERT_EQ(some_gpio_port, 1);
+	ASSERT_EQ(some_gpio_port, (uint32_t) init_val | (1 << pin));
 	PASS();
 }
 
-TEST test_write_spi_gpio_pin_0_low(void)
+TEST test_write_spi_gpio_pins_low(unsigned int pin)
 {
-	uint32_t initial_val = 0xFFFFFFFF;
-	some_gpio_port = initial_val;
-	unsigned int pin = 0; // vals 0-15
+	uint32_t init_val = 0x1000FFFF;
+	some_gpio_port = init_val;
 	deassert_spi_pin(&some_gpio_port, pin);
-	ASSERT_EQ(some_gpio_port, initial_val & ~(1 << pin));
+	ASSERT_EQ(some_gpio_port, (uint32_t) init_val & ~(1 << pin));
 	PASS();
 }
 
-TEST test_write_spi_gpio_pin_3_high(void)
+TEST test_snprintf_return_val(bool sn_error)
 {
-	uint32_t initial_val = 5;
-	some_gpio_port = initial_val;
-	unsigned int pin = 3;
-	assert_spi_pin(&some_gpio_port, pin);
-	ASSERT_EQ(some_gpio_port, initial_val + (1 << pin));
+	ASSERT_FALSE(sn_error);
 	PASS();
+}
+
+void test_all_valid_gpio_pins_set_high(void)
+{
+	for (int i = 0; i < 16; ++i) {
+		char test_suffix[5];
+		int sn = snprintf(test_suffix, 4, "%u", i);
+		bool sn_error = (sn > 5) || (sn < 0);
+		greatest_set_test_suffix((const char*) &test_suffix);
+		RUN_TEST1(test_snprintf_return_val, sn_error);
+		greatest_set_test_suffix((const char*) &test_suffix);
+		RUN_TEST1(test_write_spi_gpio_pins_high, i);
+	}
+}
+
+void test_all_valid_gpio_pins_set_low(void)
+{
+	for (int i = 0; i < 16; ++i) {
+		char test_suffix[5];
+		int sn = snprintf(test_suffix, 4, "%u", i);
+		bool sn_error = (sn > 5) || (sn < 0);
+		greatest_set_test_suffix((const char*) &test_suffix);
+		RUN_TEST1(test_snprintf_return_val, sn_error);
+		greatest_set_test_suffix((const char*) &test_suffix);
+		RUN_TEST1(test_write_spi_gpio_pins_low, i);
+	}
 }
 
 SUITE(spi_driver)
 {
-	RUN_TEST(test_write_spi_gpio_pin_0_high);
-	RUN_TEST(test_write_spi_gpio_pin_0_low);
-	RUN_TEST(test_write_spi_gpio_pin_3_high);
+	// looped test
+	test_all_valid_gpio_pins_set_high();
+	test_all_valid_gpio_pins_set_low();
 }
 
