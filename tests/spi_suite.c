9b9f3d56b27a52693d9fdd871a814f26e8653ac2
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-19 09:05:03 +0000

    Add tests for out of bounds pin values
    - Note these tests now fail

--- tests/spi_suite.c
+++ tests/spi_suite.c
@@ -79,6 +79,28 @@ void test_all_valid_gpio_pins_set_low(void)
 	}
 }
 
+TEST test_out_of_bounds_gpio_pins_write_high_no_effect(unsigned int pin)
+{
+	uint32_t init_val = 0x00000000;
+	some_gpio_port = init_val;
+	assert_spi_pin(&some_gpio_port, pin);
+	ASSERT_EQ(some_gpio_port, init_val);
+	PASS();
+}
+
+void test_out_of_bounds_gpio_pins_set_high(void)
+{
+	for (int i = 16; i < 100; ++i) {
+		char test_suffix[5];
+		int sn = snprintf(test_suffix, 4, "%u", i);
+		bool sn_error = (sn > 5) || (sn < 0);
+		greatest_set_test_suffix((const char*) &test_suffix);
+		RUN_TEST1(test_snprintf_return_val, sn_error);
+		greatest_set_test_suffix((const char*) &test_suffix);
+		RUN_TEST1(test_out_of_bounds_gpio_pins_write_high_no_effect, i);
+	}
+}
+
 SUITE(spi_driver)
 {
 	RUN_TEST(test_write_spi_gpio_pin_7_high);
@@ -86,5 +108,6 @@ SUITE(spi_driver)
 	// looped test
 	test_all_valid_gpio_pins_set_high();
 	test_all_valid_gpio_pins_set_low();
+	test_out_of_bounds_gpio_pins_set_high();
 }
 
