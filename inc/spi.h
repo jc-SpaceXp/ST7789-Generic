b7a0683631194fb563bb861519263c429cb3d567
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-21 18:03:03 +0000

    Add volatile qualifier to spi assert/deassert functions

--- inc/spi.h
+++ inc/spi.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-void assert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
-void deassert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin);
+void assert_spi_pin(volatile uint32_t* gpio_output_addr, unsigned int gpio_pin);
+void deassert_spi_pin(volatile uint32_t* gpio_output_addr, unsigned int gpio_pin);
 
 void trigger_spi_transfer(uint16_t* spi_tx_reg, uint16_t data);
 
--- src/spi.c
+++ src/spi.c
@@ -8,13 +8,13 @@ static uint32_t pin_to_bit_pos_conversion(unsigned int pin)
 	return (1 << pin);
 }
 
-void assert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin)
+void assert_spi_pin(volatile uint32_t* gpio_output_addr, unsigned int gpio_pin)
 {
 	if (gpio_pin > LAST_PIN) { return; }
 	*gpio_output_addr |= pin_to_bit_pos_conversion(gpio_pin);
 }
 
-void deassert_spi_pin(uint32_t* gpio_output_addr, unsigned int gpio_pin)
+void deassert_spi_pin(volatile uint32_t* gpio_output_addr, unsigned int gpio_pin)
 {
 	if (gpio_pin > LAST_PIN) { return; }
 	*gpio_output_addr &=  ~pin_to_bit_pos_conversion(gpio_pin);
