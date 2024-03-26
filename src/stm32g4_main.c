b00d603873e1b91cfab2098b922c1aa692385e43
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-26 18:09:39 +0000

    Demonstrate flashing LED with st7789 pin struct and spi code

--- src/stm32g4_main.c
+++ src/stm32g4_main.c
@@ -1,7 +1,9 @@
 #include "stm32g4_main.h"
 #include "stm32g4xx_spi.h"
+#include "stm32g4xx_timers.h"
 #include "spi.h"
 #include "st7789.h"
+#include "st7789_private.h"
 
 #include "stm32g4xx_hal.h"
 //#include "stm32g4xx_nucleo.h"
@@ -15,13 +17,18 @@ int main (void)
 {
 	HAL_Init();
 
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV8;
 	// Setup LED
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
 	GPIOB->MODER &= ~((LD2_BIT0) | (LD2_BIT1));
 	GPIOB->MODER |= (LD2_BIT0);
+	struct St7789SpiPin ld2_pin = { &GPIOB->BSRR, &GPIOB->BRR, LD2_PIN };
 
 	for (;;) {
-		assert_spi_pin(&GPIOB->BSRR, LD2_PIN); // set LE2
+		assert_spi_pin(ld2_pin.assert_addr, ld2_pin.pin); // set
+		HAL_Delay(40);
+		assert_spi_pin(ld2_pin.deassert_addr, ld2_pin.pin); // clear
+		HAL_Delay(40);
 	}
 
 	return 0;
